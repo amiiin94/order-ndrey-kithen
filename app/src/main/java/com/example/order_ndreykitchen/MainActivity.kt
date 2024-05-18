c738d4bb914d3088b36bfbfc85ca0d6d0bb9dfca
amiiin94 <amin942004@gmail.com>
2024-05-18 18:45:53 +0700

    solve bug in navbar and change order icon

--- app/src/main/java/com/example/order_ndreykitchen/MainActivity.kt
+++ app/src/main/java/com/example/order_ndreykitchen/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         // Apply window insets to the root layout
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
 
